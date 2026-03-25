b06bb0b9ba2089396cd70ef6490fad11f742d0d7
Vítor Cavalcante Souza <51930276+Vitor-C-Souza@users.noreply.github.com>
2026-03-25 19:11:12 -0300

    Create the `ApplyButton` component for the job details screen
    - Implement `ApplyButton` featuring a custom linear gradient background and rounded corners.
    - Add "Apply Now" text with specific styling for the button's label.
    - Include a Composable preview to facilitate UI development.

---
+++ app/src/main/java/br/me/vitorcsouza/jobfydev/ui/components/details/ApplyButton.kt
@@ -0,0 +1,65 @@
+package br.me.vitorcsouza.jobfydev.ui.components.details
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.consumeWindowInsets
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import br.me.vitorcsouza.jobfydev.ui.theme.JobfyDevTheme
+
+@Composable
+fun ApplyButton(
+    onClick: () -> Unit = {},
+    modifier: Modifier = Modifier
+) {
+    val gradientBrush = Brush.linearGradient(
+        colors = listOf(
+            Color(0xFF0B3C5D),
+            Color(0xFF165A8F)
+        )
+    )
+
+    Button(
+        onClick = onClick,
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(44.dp)
+            .clip(RoundedCornerShape(16.dp))
+            .background(gradientBrush),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.Transparent,
+            contentColor = Color.White
+        ),
+        contentPadding = ButtonDefaults.ContentPadding,
+        elevation = ButtonDefaults.buttonElevation(
+            defaultElevation = 0.dp,
+            pressedElevation = 0.dp,
+        )
+    ) {
+        Text(
+            text = "Apply Now",
+            color = Color.White,
+            fontSize = 18.sp
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun ApplyButtonPreview() {
+    JobfyDevTheme {
+        ApplyButton()
+    }
+}
